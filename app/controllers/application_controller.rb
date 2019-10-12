77e845fca845bb9173dc45577816664f5cd7f771
Eyal Toledano <eutait@gmail.com>
2019-10-12 18:10:53 -0400

    Prepares the partials folder structure. Adds the home logged in/out partials. Adds helper methods to application controller in preparation of user auth. Sets extract_css: true in webpacker.yml to prevent flash of unstyled content.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,27 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user
+  helper_method :logged_in?
+  helper_method :current_user
+  helper_method :redirect_if_not_logged_in
+
+  def current_user
+    User.find(session[:user_id])
+  end
+
+  def logged_in?
+    session[:user_id].present?
+  end
+
+  def set_current_user
+    if logged_in?
+      @user = User.find(session[:user_id])
+    end
+  end
+
+  def redirect_if_not_logged_in
+    if !logged_in?
+      flash[:notice] = "You need to be logged in to access this page."
+      redirect_to login_path
+    end
+  end
 end
