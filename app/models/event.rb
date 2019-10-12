1a410a67c7e6e1045bb53d7e2e485ecb7920c6ee
Eyal Toledano <eutait@gmail.com>
2019-10-12 01:20:23 -0400

    Installs Tailwind.css, populates the default tailwind config and create a pages controller and home page.

--- app/models/event.rb
+++ app/models/event.rb
@@ -6,6 +6,16 @@ class Event < ApplicationRecord
   def available_tickets
     tickets = []
     self.tickets.each do |ticket|
+      # Can control what the available_ticket looks like here if needed:
+      #
+      # serialized_ticket = {
+      #   id: ticket.id,
+      #   price: ticket.price,
+      #   description: ticket.description,
+      #   status: ticket.status
+      # }
+      # tickets << serialized_ticket if ticket.status == "Available for purchase"
+      # 
       tickets << ticket if ticket.status == "Available for purchase"
     end
     tickets
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
 
+  root 'pages#home'
+
   resources :brands
   resources :events do
     resources :tickets
   end
   resources :venues
-  
+
 end
