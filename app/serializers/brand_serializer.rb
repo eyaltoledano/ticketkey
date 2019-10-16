24ea6b9e18cd875af1f57b3765a106ed9c531883
Eyal Toledano <eutait@gmail.com>
2019-10-16 10:23:03 -0400

    Reversed the order of a brand's events to prepare for adding newly created events to the DOM (at the top of the array).

--- app/serializers/brand_serializer.rb
+++ app/serializers/brand_serializer.rb
@@ -2,4 +2,8 @@ class BrandSerializer < ActiveModel::Serializer
   attributes :id, :name, :description
   has_many :events
   has_many :tickets, through: :events
+
+  def events
+    object.events.sort.reverse!
+  end
 end
